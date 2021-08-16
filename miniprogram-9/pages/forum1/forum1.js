4dbc021353ee2980a11bc3c96169f7cfecb5715a
Ereshkigal0386 <88971886+Ereshkigal0386@users.noreply.github.com>
2021-08-16 16:53:39 +0800

    2021-8-16 16:53:25 update

--- miniprogram-9/pages/forum1/forum1.js
+++ miniprogram-9/pages/forum1/forum1.js
@@ -1,33 +1,11 @@
-// pages/homepage/homepage.js
+// pages/forum1/forum1.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    InfoList:[
-      {
-        index:0,
-        title:"学习资源|留学君带你了解留学三步骤",
-        date:"2021年8月16日",
-        signature:"工大留学君",
-        img:"../../images/test02.jpg"
-      },
-      {
-        index:1,
-        title:"讲座报名|大三学姐XXX分享美国留学经验",
-        date:"2021年8月8日",
-        signature:"北京工业大学经管学院学生会",
-        img:"../../images/test02.jpg"
-      },
-      {
-        index:2,
-        title:"xxxx|balabalabalabala",
-        date:"2021年x月x日",
-        signature:"北京工业大学某某某",
-        img:"../../images/test02.jpg"
-      }
-    ]
+
   },
 
   /**
